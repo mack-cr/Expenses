32f72a6ec9648d81d8a316d2461f8a4c6afcdf66
CMack <chante.r.mack@gmail.com>
2020-05-06 13:25:20 -0400

    Add project files.

---
+++ ExpensesReportPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expenses
+{
+    /// <summary>
+    /// Interaction logic for ExpensesReportPage.xaml
+    /// </summary>
+    public partial class ExpensesReportPage : Page
+    {
+        public ExpensesReportPage()
+        {
+            InitializeComponent();
+        }
+
+        // Custom constructor to pass expense report data
+        public ExpensesReportPage(object data) : this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+    }
+}
